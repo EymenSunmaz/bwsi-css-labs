103038a1efa3afbf85d5f969ce30723df8ab031b
EymenSunmaz <eymensunmaz4545@gmail.com>
2026-02-07 21:18:55 +0300

    Add user santization

--- labs/lab_1/lab_1b.py
+++ labs/lab_1/lab_1b.py
@@ -36,14 +36,21 @@ def simple_calculator(operation: str, num1: float, num2: float) -> float:
             raise ValueError("Cannot divide by zero.")
     else:
         raise ValueError("Invalid operation. Please choose from 'add', 'subtract', 'multiply', or 'divide'.")
+def requeste_sanitize_input(prompt):
+    while True:
+        try:
+            number = float(input(prompt))
+            return number
+        except ValueError:
+            print("Please enter a valid number.")
 
 def main():
     
     print(f"===== Simple Calculator =====")
 
     # Ask the user for sample input    
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
+    num1 = requeste_sanitize_input("Enter the first number: ")
+    num2 = requeste_sanitize_input("Enter the second number: ")
     operation = input("Enter the operation (add, subtract, multiply, divide): ").strip().lower()
 
     # Perform the calculation and display the result
